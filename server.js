e8b77940662bbde6b09e5890d6593a39abf8d4e5
initialsky0 <initialsky@protonmail.com>
2020-07-05 16:30:02 -0400

    display welcome

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//    res.send('Welcome to SmartBrain');
-// });
+app.get('/', (req, res) => {
+   res.send('Welcome to SmartBrain');
+});
 
 // app.post('/signin', (req, res) => { signin.handleSignin(req, res, bcrypt, smartBrainDB) }); The same as below
 app.post('/signin', signin.handleSignin(bcrypt, smartBrainDB));
