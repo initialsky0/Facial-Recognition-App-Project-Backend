efa177d9174c35d36b0a5878e2cbdb3548fdacdd
initialsky0 <initialsky@protonmail.com>
2020-07-05 17:15:51 -0400

    testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 const smartBrainDB = knex({
    client: 'pg',
    connection: {
-     host : process.env.DATABASE_URL,
+     connectionString: process.env.DATABASE_URL,
      ssl: {
       rejectUnauthorized: false
     }
